8992a223bff1f20f9c4a381c6fe53281c17817ae
Adam <runnirr@gmail.com>
2014-01-08 20:55:36 -0800

    Change name to XVoicePlus

--- Android.mk
+++ Android.mk
@@ -8,7 +8,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PACKAGE_NAME := VoicePlus
+LOCAL_PACKAGE_NAME := XVoicePlus
 LOCAL_CERTIFICATE := platform
 
 include $(BUILD_PACKAGE)
